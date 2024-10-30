75a758b6312326a6e14c8cbe19a2c0b6b217c759
Adrien <adrien.podsiadly@protonmail.com>
2024-10-30 23:19:29 +0100

    Implemented streams page\ - Added navbar - Implemented basic UI

--- src/Controller/TwitchController.php
+++ src/Controller/TwitchController.php
@@ -9,19 +9,35 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class TwitchController extends AbstractController
 {
+
     private function getOauthToken() {
-       // 
+        
+        $secret = $_ENV['CLIENT_SECRET'];
+        $clientId = $_ENV['CLIENT_ID'];
+        $client = new Client();
+        $response = $client->request("POST", "https://id.twitch.tv/oauth2/token?client_id=". $clientId ."&client_secret=". $secret . "&grant_type=client_credentials", [
+            'headers' => [
+                'Content-Type' => 'application/x-www-form-urlencoded'
+                ]
+        ]);
+        
+        $token = json_decode($response->getBody(), true);
+        return $token;
     }
 
     #[Route('/streams', name: 'app_twitch')]
     public function getStreams(): Response
     {
+        $clientId = $_ENV['CLIENT_ID'];
+        $token = $this->getOauthToken();
+        
         $client = new Client();
         $response = $client->request(
             "GET",
-            "https://api.twitch.tv/helix/streams",
+            "https://api.twitch.tv/helix/streams?first=100",
             ['headers' => [
-                'ClientId' => ''
+                'Authorization' => 'Bearer ' . $token['access_token'],
+                'Client-Id' => $clientId
             ]],
         );
 
